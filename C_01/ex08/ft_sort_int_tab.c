999f2183cbde83c1bfdc44413c09f29ee56f9341
sua1223 <70890638+sua1223@users.noreply.github.com>
2021-10-11 07:21:08 -0700

    solved 7,8

---
+++ C_01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+
+void	ft_swap(int *a, int *b)
+{
+	int	tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < size - 1)
+	{
+		j = 0;
+		while (j < size - 1 - i)
+		{
+			if (tab[j] > tab[j + 1])
+			{
+				ft_swap(&tab[j], &tab[j + 1]);
+			}
+			j++;
+		}
+		i++;
+	}
+}
